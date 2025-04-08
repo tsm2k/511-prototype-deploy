7da3afe05b10ee9df589366e7e00b0ebca2ada10
tanay3k <tanay@Tanays-MacBook-Air.local>
2025-04-08 05:07:27 -0400

    stable version

--- next.config.js
+++ next.config.js
@@ -7,4 +7,17 @@ module.exports = {
       ignoreDuringBuilds: true,
     },
   }
+
+const repo = '511-prototype-deploy' // <- your GitHub repo name
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
+  
   
